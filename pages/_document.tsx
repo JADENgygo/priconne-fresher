4e1d264fb5a4e5b666c62cff80293686dbae317a
JADEN <29948746+JADENgygo@users.noreply.github.com>
2022-03-20 15:06:39 +0900

    fix

---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="プリコネRのガチャ告知画像の作成ツール"
+          />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@JADENgygo" />
+          <meta
+            property="og:url"
+            content="https://priconne-fresher.vercel.app"
+          />
+          <meta property="og:title" content="プリコネフレッシャー" />
+          <meta
+            property="og:description"
+            content="プリコネRのガチャ告知画像の作成ツール"
+          />
+          <meta
+            property="og:image"
+            content="https://priconne-fresher.vercel.app/img/peko.png"
+          />
+          <link rel="icon" href="/img/peko.png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap"
+            rel="stylesheet"
+          />
+          <title>プリコネフレッシャー</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
